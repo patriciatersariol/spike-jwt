2f0c637704ebef737ed481492ca64f5940c7cf18
Bianca Salino <biancadsalino@gmail.com>
2021-11-08 14:38:36 -0300

    ajustes do back

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require("dotenv").config()
-
+//import cors from 'cors'
+const cors = require('cors')
 const express = require("express")
 const app = express()
 
@@ -9,31 +10,30 @@ const app = express()
 const jwt = require("jsonwebtoken")
 
 app.use(express.json())
-
-const posts = [
-  {
-    username: "Patrícia",
-    title: "Post 1"
-  },
-  {
-    username: "Bianca",
-    title: "Post 2"
-  }
-]
+app.use(cors({ origin: '*' }))
+const page = 'Página autorizada!'
 
 
-app.get("/posts", authenticateToken, (req, res) => {
-  res.json(posts.filter(post => post.username === req.user.name))
+app.get("/page", authenticateToken, (req, res) => {
+  res.json(page)
 })
 
 app.post("/login", (req, res) => {
-  // Autenticação
+
+  const user = { email: req.body.email, password: req.body.password }
+  const userAdmin = { email: 'ju@gmail.com', password: 1234 }
+
+  if (user.email == userAdmin.email && user.password == userAdmin.password) {
+    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
+    return res.json({ accessToken: accessToken })
+  }
+
+  return res.json({ message: 'usuário inválido' })
   //autenticacao é garantir que vocÊ é vocÊ mesmo
   //autorização é garantir que vocÊ tem permissao para fazer alguma coisa dentro da aplicação 
 
   //envia no body da requisiçao os dados 
-  const username = req.body.username
-  const user = { name: username }
+
   //se o login bateu, vai ser "assinado" um JWT em três partes
   //algumas informações do JWT
   //payload que são algumas informações que permite identificar alguas informações desse usuário depois
@@ -42,29 +42,33 @@ app.post("/login", (req, res) => {
   //1º passa o payload: alguma informação que permite identificar o usuário qual é o usuário que ta fazendo requisição, ideal é nao colocar muitos dados e nem dados sensíveis para nao expor informações da sua aplicação
   //depois de verificar no banco de dados que existe, retorna um ID e coloca dentro do token para que quando novas requições forem feitas poder pegar o token jwt  e verificar que essa pessoa quer acessar 
   //passa a secret que é a senha utilizada para assinatura digital essa senha pode guardar numa variavel de ambiente onde quiser 
-  const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET) //retorna um objeto token
+  //retorna um objeto token
 
   // na resposta da requisição passa a variavel token que o front ou usuario vai ter que guardar porque toda requição vai ter uqe mandar esse token de novo
-  res.json({ accessToken: accessToken })
+
 
 })
 
 // vai esperar por parametro req ou res 
 //funciona como um middlewere 
 function authenticateToken(req, res, next) {
+
   //vai pegar um token que ta no req.headers e passar o cabeçalho que quer 
   //qualquer cabeçalho funciona (authorization ou x-acess-token)
   const authHeader = req.headers["authorization"]
+
   const token = authHeader && authHeader.split(" ")[1]
-  if (token == null) return res.sendStatus(401)
+  if (token === null) return res.sendStatus(401)
 
   //usar verify para verificar o token passando o secret, usa para assinar e verificar a validade de um token
   //terceiro parametro é uma callback que pode ter erro ou o token decodificado
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-    if (err) return res.sendStatus(403) //se der erro retorna token invalido ou ausente
-    req.user = user //caso contrario  o objeto user vem preenchido com o payload do jwt informaçoes do usuario 
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+
+    if (err) return res.sendStatus(401) //se der erro retorna token invalido ou ausente
+    //req.user.email = decoded.email
+    //caso contrario  o objeto user vem preenchido com o payload do jwt informaçoes do usuario 
     next() // executa a proxima funcao das camadas de middlewere do express
   })
 }
 
-app.listen(3000)
+app.listen(9000)
